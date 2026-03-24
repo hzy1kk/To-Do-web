d50354f4d5090399d2463491fef6094214548f1c
hzy1kk <lucaslohan@colegiopaulodetarso.com.br>
2026-03-24 14:04:16 +0000

    .

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const taskBtn = document.querySelector("#taskBtn")
 const taskList = document.querySelector("#taskList")
 const taskInput = document.querySelector("#taskInput")
 
-taskBtn.addEventListener("click", function(){
-    alert("CLIQUEI !!!")
+  taskBtn.addEventListener("click", function(){
+    const li = document.createElement('li');
+    const taskInputValue = taskInput.value;
+    li.textContent = taskInputValue;
+    taskList.appendChild(li)
 })
