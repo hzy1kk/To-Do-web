1c9c1174716c18b0d28f4d46d88c63ed1d39390c
hzy1kk <lucaslohan@colegiopaulodetarso.com.br>
2026-03-24 14:25:43 +0000

    .2

--- script.js
+++ script.js
@@ -2,18 +2,27 @@ const taskBtn = document.querySelector("#taskBtn");
 const taskList = document.querySelector("#taskList");
 const taskInput = document.querySelector("#taskInput");
 
+function addDeleteFunction(span, li) {
+  span.addEventListener("click", function() {
+    taskList.removeChild(li);
+  });
+}
+
+document.querySelectorAll(".deleteSpan").forEach(span => {
+  const li = span.parentElement;
+  addDeleteFunction(span, li);
+});
+
 taskBtn.addEventListener("click", function() {
   const taskInputValue = taskInput.value.trim();
   if (taskInputValue === "") return;
   const li = document.createElement("li");
   li.textContent = taskInputValue;
-  const deleteBtn = document.createElement("button");
-  deleteBtn.textContent = "Excluir";
-  deleteBtn.style.marginLeft = "10px";
-  deleteBtn.addEventListener("click", function() {
-    taskList.removeChild(li);
-  });
-  li.appendChild(deleteBtn);
+  const deleteSpan = document.createElement("span");
+  deleteSpan.textContent = "Excluir";
+  deleteSpan.className = "deleteSpan";
+  addDeleteFunction(deleteSpan, li);
+  li.appendChild(deleteSpan);
   taskList.appendChild(li);
   taskInput.value = "";
 });
